5dab955dbc47f83cd0b75e695e639e43b38bbbdc
guile <guile@flyskyler.com>
2023-11-22 13:15:30 +0100

    updated sampling script

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,7 +1,57 @@
+#!/bin/bash
+
+# usage: ./process.sh  [video_path] [output_path] [images_path] [sample_size]
+
+VIDEO_PATH=$1
+OUTPUT_PATH=$2
+IMAGES_PATH=$3
+SAMPLE_SIZE=$4
+
+# Check if the source and destination folders are provided
+if [ -z "$VIDEO_PATH" ]; then
+    echo "Video path not provided"
+    exit 1
+fi
+if [ -z "$OUTPUT_PATH" ]; then
+    echo "Output path not provided"
+    exit 1
+fi
+if [ -z "$IMAGES_PATH" ]; then
+    echo "Images path not provided"
+    exit 1
+fi
+if [ -z "$SAMPLE_SIZE" ]; then
+    echo "Images path not provided"
+    exit 1
+fi
+
 mkdir _tmp;
-mkdir $2;
 mkdir _tmp/images;
-ffmpeg -i $1 _tmp/images/%04d.jpg;
-colmap automatic_reconstructor --workspace_path ./_tmp --image_path ./_tmp/images;
-colmap model_converter --input_path ./_tmp/sparse/0 --output_path ./$2/ --output_type TXT;
+mkdir _tmp/sample_images;
+mkdir $OUTPUT_PATH;
+
+# extracting frames from  video
+ffmpeg -i "$VIDEO_PATH" ./_tmp/images/%04d.jpg;
+
+# sampling images from folder
+find ./_tmp/images -type f | shuf -n "$SAMPLE_SIZE" | xargs -I {} cp {} ./_tmp/sample_images
+
+# running colmap
+colmap automatic_reconstructor \
+    --workspace_path ./_tmp \
+    --image_path ./_tmp/sample_images \
+    --camera_model SIMPLE_RADIAL;
+
+# converting colmap output
+colmap model_converter \
+    --input_path ./_tmp/sparse/0 \
+    --output_path ./$OUTPUT_PATH/ \
+    --output_type TXT;
+
+# copying the images to the desired folder
+mkdir $IMAGES_PATH;
+cp _tmp/images/* $IMAGES_PATH;
+
+# cleanup
 rm -r _tmp;
+
