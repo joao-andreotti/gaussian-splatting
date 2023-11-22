fc1f117177f7c056cba62a98333e65b12916404c
joao-andreotti <jvmandreotti@usp.br>
2023-11-22 15:31:31 +0100

    implement ssim loss using pytorch

---
+++ src/gaussian_splatting/loss.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn.functional as F
+
+
+def create_quare_window(window_size: int, channels: int) -> torch.Tensor:
+    """Creates a square window for averaging."""
+    window = torch.ones(
+        (channels, 1, window_size, window_size), dtype=torch.float, device="cuda"
+    )
+    return window / (window_size * window_size)
+
+
+def DSSIM(
+    original_image: torch.Tensor,
+    rendered_image: torch.Tensor,
+    *,
+    window_size: int = 11,
+    data_range: float = 1.0
+) -> torch.Tensor:
+    """Computes the structural similarity index between two images.
+    Adapted from: Po-Hsun-Su/pytorch-ssim.git
+    """
+    channels = original_image.shape[0]
+    window = create_quare_window(window_size, channels=channels)
+
+    mu1 = F.conv2d(original_image, window, padding="same", groups=channels)
+    mu2 = F.conv2d(rendered_image, window, padding="same", groups=channels)
+
+    mu1_sq = mu1.square()
+    mu2_sq = mu2.square()
+
+    mu1_mu2 = mu1 * mu2
+
+    sigma1_sq = (
+        F.conv2d(
+            original_image * original_image, window, padding="same", groups=channels
+        )
+        - mu1_sq
+    )
+    sigma2_sq = (
+        F.conv2d(
+            rendered_image * rendered_image, window, padding="same", groups=channels
+        )
+        - mu2_sq
+    )
+    sigma12 = (
+        F.conv2d(
+            original_image * rendered_image, window, padding="same", groups=channels
+        )
+        - mu1_mu2
+    )
+
+    C1 = (0.01 * data_range) ** 2
+    C2 = (0.03 * data_range) ** 2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / (
+        (mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2)
+    )
+    return 1 - ssim_map.mean()
